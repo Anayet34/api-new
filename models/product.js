f2dc89d3768ec663ef6290e4429034eec67100b7
anayet34 <anayetsarker40@gmail.com>
2023-02-11 17:54:27 +0600

    api

---
+++ models/product.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    featured: {
+        type: Boolean,
+        default: false,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    company: {
+        type: String,
+        required: true
+        // enum: {
+        //     values:["apple", "samsung", "dell", "mi"],
+        //     message: `{value} is not supported ` ,
+        // },
+    },
+});
+
+module.exports = mongoose.model("Product", productSchema);
